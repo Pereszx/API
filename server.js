89f6d037d3afbc2fdfc658dfe9e62a83292eace2
Kaua Peres <kauaperessdure13@gmail.com>
2024-12-01 23:15:04 -0300

    API

---
+++ server.js
@@ -0,0 +1,81 @@
+/* API de usuarios
+
+  - Criar usuário
+  - Listar todos os usuarios
+  - Editar um usuário
+  - Deletar um usuário
+
+
+  kaua 100222Kb - MONGODB
+*/
+
+import express from 'express'
+import cors from 'cors'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+
+app.post('/usuarios', async (req, res) => {
+
+  await prisma.user.create({
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  })
+
+  res.status(201).json(req.body)
+})
+
+app.get('/usuarios', async (req, res) => {
+
+  let users = []
+
+  if (req.query) {
+    users = await prisma.user.findMany({
+      where: {
+        name: req.query.name,
+        email: req.query.email,
+        age: req.query.age
+      }
+    })
+  } else {
+    users = await prisma.user.findMany()
+  }
+
+  res.status(200).json(users)
+})
+
+app.put('/usuarios/:id', async (req, res) => {
+
+  await prisma.user.update({
+    where: {
+      id: req.params.id
+    },
+    data: {
+      email: req.body.email,
+      name: req.body.name,
+      age: req.body.age
+    }
+  })
+
+  res.status(201).json(req.body)
+})
+
+app.delete('/usuarios/:id', async (req, res) => {
+  await prisma.user.delete({
+    where: {
+      id: req.params.id
+    }
+  })
+
+  res.status(200).json({ message: 'Usuário deletado com sucesso!' })
+})
+
+app.listen(3000)
